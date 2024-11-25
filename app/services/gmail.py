914ed16f5070496364ad924789d8b53f03ff5418
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2024-11-24 23:47:35 -0600

    changes in gmail api

--- app/services/gmail.py
+++ app/services/gmail.py
@@ -22,7 +22,7 @@ def auth_gmail():
                                                        access_type="online", prompt="consent")
     session['gmail_state'] = state
 
-    return redirect(authorization_url)
+    return jsonify({"authorizationUrl": authorization_url})
 
 
 def auth_gmail_callback():

d0d8fb941dfc4357df37a53682e00bcf1f59378f
Luis Galea <luisgalea31@gmail.com>
2025-04-03 17:29:40 -0400

    feat<>: add setup_referrals_routes

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from app.routes.proxyRoutes import setup_proxy_routes
 from app.routes.core.principal_ia import setup_routes_chats
 from app.routes.executeRoutes import setup_execute_routes
 from app.routes.refreshTokens import setup_routes_refresh
+from app.routes.referralsRoutes import setup_referrals_routes
 from flask_caching import Cache  # Importar Flask-Caching
 
 load_dotenv()
@@ -21,24 +22,25 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
     # Configurar Redis y caché
     app.config['CACHE_TYPE'] = 'RedisCache'
     app.config['CACHE_REDIS_URL'] = os.getenv("REDIS_URL")  # Obtén la URL de Redis desde las variables de entorno
     app.config['CACHE_DEFAULT_TIMEOUT'] = 1800  # Tiempo de expiración (30 minutos)
-
+    
     cache = Cache(app)  # Inicializa el objeto de caché
     CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-
+    
     # Configurar MongoDB
     app.config['MONGO_URI'] = os.getenv('MONGO_URI')
     mongo_client = PyMongo(app).db  # Mantiene la conexión
     mongo = mongo_client["Prueba"]  # Obtiene directamente la colección
-
+    
     try:
         print("Conexión exitosa a MongoDB!")
     except Exception as e:
         print("Error al conectar con MongoDB:", e)
+    
     setup_routes(app, mongo)
     setup_auth_routes(app, mongo, cache)
     setup_user_routes(app, mongo, cache)
@@ -48,11 +50,12 @@ def create_app():
     setup_proxy_routes(app, mongo, cache)
     setup_routes_chats(app, mongo, cache, refresh_functions)
     setup_execute_routes(app, mongo, cache, refresh_functions)
+    setup_referrals_routes(app, mongo, cache)
     
     return app
 
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render asigna un puerto en la variable PORT
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
